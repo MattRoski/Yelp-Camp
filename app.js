c19a63f7554d962801e88385126143063bd2f0c0
MattRoski <mattroski88@gmail.com>
2023-01-04 14:48:18 -0600

    Added Review schema. Added Review form. Can now add reviews on a camp. Added validation for reviews

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Joi = require('joi');
-const {campgroundSchema} = require('./schemas.js');
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
+const review = require('./models/review');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -47,6 +49,16 @@ const validateCampground = (req, res, next) => {
        } else{
         next();
        }
+}
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(','); //returns a signle new string that we join
+        throw new ExpressError(msg,400);
+       } else{
+        next();
+       }    
 }
 
 app.get('/campgrounds', catchAsync(async (req,res) =>{
@@ -88,6 +100,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res) =>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) =>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 //app.all for every single request. and * for every path
 app.all('*', (req, res, next) =>{
